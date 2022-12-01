1e0042a1711f5f3391efc173b1c6a50ef05c85ea
Dmytro Myrhorodskyi <enigmatic992@gmail.com>
2022-12-01 18:08:06 +0200

    Adding functionality from Dmytro

--- dima_module.py
+++ dima_module.py
@@ -1,2 +1,24 @@
-def func():
-    return "This is Dima's module"
+def movie_finder(genre, year):
+    res = []
+
+    with open('movies.txt', 'r') as f:
+        for line in f:
+            movie_line = line.strip().split(';')
+
+            movie = {
+                'title': movie_line[0],
+                'genres': ', '.join(movie_line[1].split(', ')),
+                'year': int(movie_line[2]),
+                'link': movie_line[3]
+            }
+
+            if genre in movie['genres'] and year == movie['year']:
+                res.append(f'{movie["title"]}\n{movie["genres"]}\n{movie["year"]}\n{movie["link"]}\n')
+
+            if len(res) >= 5:
+                break
+
+    if len(res) > 0:
+        return '\n'.join(res)
+    else:
+        return 'По вашим критеріям нічого не знайдено.'
--- main.py
+++ main.py
@@ -15,9 +15,35 @@ def message_start(message):
     bot.send_message(message.chat.id, 'Hello, user!')
 
 
-@bot.message_handler(commands=['dima'])
-def dima_func(message):
-    bot.send_message(message.chat.id, dima_module.func())
+# ===== DIMA'S FUNCTIONS START =====
+user_choices = {}
+
+
+@bot.message_handler(commands=['dima', 'movie'])
+def movie(message):
+    bot.send_message(message.chat.id, '''Жанр ['бойовик', 'драма', 'мелодрама', 'фантастика', 'фентезі', 'пригоди', 
+            'мультфільм', 'комедія', 'сімейний', 'детектив', 'військовий', 'біографія', 
+            'кримінал', 'трилер', 'спорт', 'музика',
+            'мюзикл', 'історія', 'аніме', 'вестерн', 'жахи']: ''')
+    bot.register_next_step_handler(message, genre_input)
+
+
+def genre_input(message):
+    print(message.text)
+    user_choices['genre'] = message.text
+    bot.send_message(message.chat.id, 'Рік [2022-2003]: ')
+    bot.register_next_step_handler(message, year_input)
+
+
+def year_input(message):
+    print(message.text)
+    user_choices['year'] = message.text
+    bot.send_message(message.chat.id, f'Ваш вибір: {user_choices["genre"]}, {user_choices["year"]}')
+    result = dima_module.movie_finder(user_choices["genre"], int(user_choices["year"]))
+    bot.send_message(message.chat.id, result)
+
+
+# ===== DIMA'S FUNCTIONS END =====
 
 
 @bot.message_handler(commands=['helga'])
